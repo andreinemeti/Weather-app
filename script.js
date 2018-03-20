94318abce2f5cecd9a112c47b1db54cda5c46cf8
Andrei Nemeti <nemetimarian@yahoo.com>
2018-03-20 15:20:46 +0200

    added the possibility to convert from celsius to farenheit

--- script.js
+++ script.js
@@ -6,33 +6,21 @@ $(function(){
     var day = date.getDate();
     var year = new Date().getFullYear();
     $("#date").text(weekday + ", " + day + " " + currentMonth + ", " + year); 
-
+    // var hour = date.getHours();
+    
 });
-
-// sky icons
-  var skycons = new Skycons({"color": "#ddd713"});
- 
-  skycons.play();
-
-  skycons.set("icon2", Skycons.CLEAR_DAY);
-
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       
-      latitude = position.coords.latitude
-      longitude = position.coords.longitude;
+      let latitude = position.coords.latitude
+      let longitude = position.coords.longitude;
       $.ajax ({  
         url: 'https://api.openweathermap.org/data/2.5/weather?lat='+ latitude +'&lon=' + longitude + '&units=metric&APPID=a550e6d882e111463620f5ce6b2666a5',
         type: 'POST',
         dataType: 'json',
         success(response) {
-            $('#city').text(response.name);
-            $('#temperature').html(response.main.temp.toFixed()  + ' ' + degree);
-            $('#weatherIcon').text(response.weather[0].main);
-            $('#wind').text(response.wind.speed + ' m/s');
-            $('#humidity').text(response.main.humidity + ' %');
-            $('#pressure').text(response.main.pressure + ' hpa');
+          getResponse(response)
         },
         error(jqXHR, status, errorThrown) {
             $('h2').text('City not found...');
@@ -42,10 +30,8 @@ $(function(){
   }
 
 
-
-
 let degree = '&#8451';   
-//&#8457 farehnheit
+
 let city = $('#searchInput');
 
 $('#searchBtn').on('click', function(){
@@ -54,14 +40,7 @@ $('#searchBtn').on('click', function(){
   type: 'POST',
   dataType: 'json',
   success(response) {
-
-  $('#city').text(response.name);
-  $('#temperature').html(response.main.temp.toFixed()  + ' ' + degree);
-  $('#weatherIcon').text(response.weather[0].main);
-  $('#wind').text(response.wind.speed + ' m/s');
-  $('#humidity').text(response.main.humidity + ' %');
-  $('#pressure').text(response.main.pressure + ' hpa');
-
+    getResponse(response)
   },
   error(jqXHR, status, errorThrown) {
     $('h2').text('City not found...');
@@ -69,12 +48,41 @@ $('#searchBtn').on('click', function(){
 });
 });
 
-let fahrenheitToCelsius = (fahrenheit) => {
-  console.log((fahrenheit - 32) * (5/9));
-  return ((fahrenheit - 32) * (5/9));
+let celsiusToFahrenheit = (celsius) => {
+  return (celsius * 9/5 + 32);
 }
 
-fahrenheitToCelsius(276);
+function fahrenheitToCelsius(fahrenheit) {
+ 
+return ((fahrenheit -32) * 5/9).toFixed(0);
+}
+
+
+function getResponse(response) {
+  $('#city').text(response.name);
+  $('#temperature').html(response.main.temp.toFixed()  + ' ' + degree);
+  $('.weatherIcon .icon').html('<img src="http://openweathermap.org/img/w/' + response.weather[0].icon + '.png'+ '"/>');
+  $('#weatherIcon').text(response.weather[0].main);
+  $('#wind').text(response.wind.speed + ' m/s');
+  $('#humidity').text(response.main.humidity + ' %');
+  $('#pressure').text(response.main.pressure + ' hpa');
+
+  
+}
+
+$('.toggleBtn').on('click',function() {
+    if (degree == '&#8451') { //if degree is celsius
+    degree = '&#8457'; //change degree to fahrenheit
+    $('#temperature').html(celsiusToFahrenheit(parseFloat($('#temperature').text()))  + ' ' + degree);
+
+    }
+    else if (degree == '&#8457') { //if degree is fahrenheit
+        degree = '&#8451'; //change degree to celsius
+        console.log(parseFloat($('#temperature').text()))
+        $('#temperature').html(fahrenheitToCelsius(parseFloat($('#temperature').text()))  + ' ' + degree);
+     }
+  });
+
 
 
 
